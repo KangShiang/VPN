646334210e2a4b8f751a1a8ba61af4e4a0697184
ianc <ian.cll+eecian@gmail.com>
2015-10-20 20:42:50 -0700

    added hooks for encrypt, decrypt, handle_first_in, handle_first_out

--- chat_app.py
+++ chat_app.py
@@ -14,11 +14,44 @@ from kivy.uix.textinput import TextInput
 from twisted.internet import reactor, protocol
 from twisted.internet.error import CannotListenError
 
+# Variables for connection establishment
+address = "127.0.0.1"
+port = "8080"
+secret = ""
+mode = "Server"
+connector = None
+connected = False
+received = False
+
+def encrypt(data):
+    # called on send
+    return data
+
+def decrypt(data):
+    # called on receive
+    return data
+
 class EchoClient(protocol.Protocol):
+    def handle_first_out(self):
+        # Called on connection made
+        # can send message with self.transport.write("str") 
+        pass
+    
+    def handle_first_in(self,data):
+        # called on first received data
+        return data
+
     def connectionMade(self):
         self.factory.app.on_connection(self.transport)
+        self.handle_first_out()
 
     def dataReceived(self, data):
+        global received
+        if not received:
+            data = self.handle_first_in(data)
+        else:
+            data = decrypt(data)
+            received = True
         self.factory.app.print_message("Other: " + data)
 
 
@@ -37,17 +70,23 @@ class EchoFactory(protocol.ClientFactory):
         self.app.print_message("connection failed")
         App.get_running_app().stop()
 
+class EchoFactoryServ(protocol.Factory):
+    protocol = EchoClient
+    
+    def __init__(self, app):
+        self.app = app
+    
+    def connectionLost(self, reason):
+        App.get_running_app().stop()
+    
+    def connectionFailed(self, reason):
+        App.get_running_app().stop()
 
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 
-# Variables for connection establishment
-address = "a"
-port = "a"
-secret = "a"
-mode = "Client"
 
 Builder.load_string('''
 <ScrollableLabel>:
@@ -65,11 +104,11 @@ class ScrollableLabel(ScrollView):
 def toggle_pressed(instance):
     global mode
     if instance.state == "normal":
-        mode = "Client"
-        instance.text = "Client"
-    else:
         mode = "Server"
         instance.text = "Server"
+    else:
+        mode = "Client"
+        instance.text = "Client"
 
 #TODO:
 def set_ip(instance, *args):
@@ -95,17 +134,17 @@ class InfoPage(Screen):
     def __init__(self, **kwargs):
         super (InfoPage, self).__init__(**kwargs)
         root = FloatLayout()
-        self.toggle = ToggleButton(text='Client', font_size=14, pos_hint={'center_x': .5, 'center_y': .90},size_hint=(None, None))
+        self.toggle = ToggleButton(text='Server', font_size=14, pos_hint={'center_x': .5, 'center_y': .90},size_hint=(None, None))
         self.toggle.bind(on_press=toggle_pressed)
 
         ip_label = Label(text='IP Address', pos_hint={'center_x': .5, 'center_y': .80}, size_hint=(None, None),
                          width=500, height=40)
-        self.ip = TextInput(text='', multiline=False, pos_hint={'center_x': .5, 'center_y': .75}, size_hint=(None, None),width=500, height=40)
+        self.ip = TextInput(text=address, multiline=False, pos_hint={'center_x': .5, 'center_y': .75}, size_hint=(None, None),width=500, height=40)
         self.ip.bind(text=set_ip)
 
         port_label = Label(text='Port', pos_hint={'center_x': .5, 'center_y': .7}, size_hint=(None, None), width=500,
                            height=40)
-        self.port = TextInput(text='', multiline=False, pos_hint={'center_x': .5, 'center_y': .65}, size_hint=(None, None), width=500, height=40)
+        self.port = TextInput(text=port, multiline=False, pos_hint={'center_x': .5, 'center_y': .65}, size_hint=(None, None), width=500, height=40)
         self.port.bind(text=set_port)
 
         secret_label = Label(text='Secret', pos_hint={'center_x': .5, 'center_y': .6}, size_hint=(None, None),
@@ -133,46 +172,65 @@ class ChatPage(Screen):
         print port
         if mode == "Server":
             try:
-                reactor.listenTCP(int(port), EchoFactory(self), interface=address)
+                connector = reactor.listenTCP(int(port), EchoFactoryServ(self), interface=address)#EchoFactory(self), interface=address)
             except CannotListenError:
                 print "Server Already Created"
                 App.get_running_app().stop()
         else:
-            reactor.connectTCP(address, int(port), EchoFactory(self))
+            connector = reactor.connectTCP(address, int(port), EchoFactory(self))
 
-    def back(self,*args):
-        self.manager.current = "InfoPage"
+    def quit(self,*args):
+        try:
+            self.connection
+            print "DISCONNECTOR"
+            self.connection.loseConnection()
+            if connector:
+                connector.disconnect()
+        except:
+            pass
+        reactor.stop()
+        #self.manager.current = "InfoPage"
+        App.get_running_app().stop()
 
     def send(self,*args):
         self.console.text = self.console.text + "\nMe: " + self.message.text
 
     def on_connection(self, connection):
+        global connected
+        if connected:
+            App.get_running_app().stop()
+        connected = True
         self.print_message("connected succesfully!")
         self.connection = connection
 
     def send_message(self, *args):
         msg = self.message.text
-        self.print_message("Me: " + msg)
-        if msg and self.connection:
-            self.connection.write(str(self.message.text))
-            self.message.text = ""
+        try:
+            self.connection
+            self.print_message("Me: " + msg)
+            if msg and self.connection:
+                self.message.text = encrypt(self.message.text)
+                self.connection.write(str(self.message.text))
+                self.message.text = ""
+        except:
+            self.print_message("Please wait for a connection.")
 
     def print_message(self, msg):
         self.console.text += msg + "\n"
 
     def __init__(self, **kwargs):
         super (ChatPage,self).__init__(**kwargs)
-        back_button = Button(text="Back", pos_hint={'center_x': .05, 'center_y': .95}, size_hint=(None, None), width=40, height=40)
-        back_button.bind(on_press=self.back)
+        self.quit_button = Button(text="Quit", pos_hint={'center_x': .05, 'center_y': .95}, size_hint=(None, None), width=40, height=40)
+        self.quit_button.bind(on_press=self.quit)
         self.console = ScrollableLabel(text="",pos_hint={'center_x': .52, 'center_y': .4})
         self.message = TextInput(multiline=True, pos_hint={'center_x': .45, 'center_y': .10}, size_hint=(None, None),
                            width=600, height=40)
-        send_button = Button(text="Send", pos_hint={'center_x': .90, 'center_y': .10}, size_hint=(None, None), width=120, height=40)
-        send_button.bind(on_press=self.send_message)
+        self.send_button = Button(text="Send", pos_hint={'center_x': .90, 'center_y': .10}, size_hint=(None, None), width=120, height=40)
+        self.send_button.bind(on_press=self.send_message)
         self.add_widget(self.console)
-        self.add_widget(back_button)
+        self.add_widget(self.quit_button)
         self.add_widget(self.message)
-        self.add_widget(send_button)
+        self.add_widget(self.send_button)
 
 # A simple kivy App, with a textbox to enter messages, and
 # a large label to display all the messages received from
