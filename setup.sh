cf07bfb7c357883742318403a3975b88f8063116
ianc <ian.cll+eecian@gmail.com>
2015-10-21 20:30:34 -0700

    updated setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get -y install python-kivy
 sudo apt-get -y install python-virtualenv
 sudo apt-get -y install python-dev
+sudo apt-get -y install libcurl4-openssl-dev
 virtualenv --system-site-packages vpnenv
 source vpnenv/bin/activate
 pip install -r PIP_REQUIREMENTS.txt
